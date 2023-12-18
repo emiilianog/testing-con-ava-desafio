54bd0bdc9e1a1cf4732e5176c993cfa4c63cc53c
Emiliano <gomez.emiiliano@gmail.com>
2023-12-18 19:28:29 -0300

    Terminé el desafio

--- test.ts
+++ test.ts
@@ -1,4 +1,20 @@
 import test from "ava";
+import { User, Product } from "./index";
 
-test("Testeo la clase User", (t) => {});
-test("Testeo la clase Product y sus métodos", (t) => {});
+test("Testeo la clase Product", (t) => {
+  const producto = new Product("Pelota", 1000);
+
+  t.is(producto.name, "Pelota");
+
+  t.is(producto.price, 1000);
+});
+
+test("Testeo la clase User y sus métodos", (t) => {
+  const nuevoUser = new User("Juancito");
+  const producto = new Product("Pelota", 1000);
+
+  t.is(nuevoUser.name, "Juancito");
+  nuevoUser.addProduct(producto);
+
+  t.is(nuevoUser.products[0], producto);
+});
